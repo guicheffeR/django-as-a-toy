f2676d60d9dd8f9e92ec4b3828b52cc495fbd850
João Guilherme <j@guiatech.com.br>
2015-04-01 18:22:12 -0300

    adding stretch for gulp aiming static files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var install = require( 'gulp-install' ),
 gulp = require( 'gulp' ),
 stylus = require( 'gulp-stylus' ) ;
- 
-gulp.src( [ './bower.json', './package.json' ] )
-	.pipe( install() ) ;
+
+/* part of installing dependencies */
+gulp.src( [ './bower.json', './package.json' ] ).pipe( install() ) ;
+
+/* STATIC FILES */
+
+
+
+/* /STATIC FILES */
 
 gulp.task( 'watch', function() {
 	//gulp.watch( './static/js/common.js', ['uglify'] ) ;
