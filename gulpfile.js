5389b4d6c4f0fb96e7180080b3a2119b71fcb18f
João Guilherme <j@guiatech.com.br>
2015-04-01 19:49:48 -0300

    setting styl in those css files

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,27 @@ gulp = require( 'gulp' ),
 stylus = require( 'gulp-stylus' ) ;
 
 /* part of installing dependencies */
-gulp.src( [ './bower.json', './package.json' ] ).pipe( install() ) ;
+gulp.src( [ './bower.json', './package.json' ] ).pipe(  install() ) ;
 
 /* STATIC FILES */
 
-
+gulp.task( 'stylus-compile', function() {
+  gulp.src( './landing/static/css/0.1/*.styl' )
+    .pipe( stylus() )
+    .pipe( gulp.dest( './landing/static/css/0.1/' ) ) ;
+});
+ 
+// Options compress 
+gulp.task( 'stylus-compress', function() {
+  gulp.src( './landing/static/css/0.1/*.styl' )
+    .pipe( stylus( {
+      compress: true
+    }))
+    .pipe( gulp.dest( './landing/static/css/0.1/' ) ) ;
+});
 
 /* /STATIC FILES */
 
 gulp.task( 'watch', function() {
-	//gulp.watch( './static/js/common.js', ['uglify'] ) ;
+	gulp.watch( './landing/static/css/0.1/*.styl', [ 'stylus-compile' ] ) ;
 } ) ;
